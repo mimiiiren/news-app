8346b2528666c7f252470fea3fd09e516052f8c5
mimiiiren <mimiren015@gmail.com>
2025-12-01 16:01:10 -0500

    updated code for better ui experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 import NewsCard from "./Components/NewsCard.jsx";
 import Form from "./Components/Form.jsx";
 
@@ -7,45 +7,40 @@ function App() {
   // news stores the data from api, which will later be .map so it must be saved as empty array
   const [news, setNews] = useState([]);
 
-  // setCategory is the setter function that updates cateogry variable. 
-  // only works with null but not "" because in fetch link "" breaks the fetch??
-  const [category, setCategory] = useState(null);
-
   const [showNoNews, setShowNoNews] = useState(true);
-  useEffect(() => {
-    // part 1 {} is what to do, the code to run
+
+  // categoryChange updates category to store received data from user in function/prop updateCategory
+  function categoryChange(updateCategory) {
     fetch(
-      `https://newsdata.io/api/1/latest?apikey=pub_bcf12d02d2704fc9bc40884b44f7eb29&q=${category}&country=us`
+      `https://newsdata.io/api/1/latest?apikey=pub_bcf12d02d2704fc9bc40884b44f7eb29&q=${updateCategory}&country=us`
     )
       .then((res) => res.json())
       .then((data) => {
         setNews(data.results);
+        // do not show no news if data fetched from api is successful
+        setShowNoNews(false);
       })
-      .catch((err) => console.log('Error fetching data:', err));
-  }, [category]);
-  // part two is [category], this is when to do it. [] means do run previous code only when page first loads. [category] means run code when page loads and when cateogry changes
-
-  // categoryChange updates category to store received data from user in function/prop updateCategory
-  function categoryChange(updateCategory) {
-    setCategory(updateCategory);
-    setShowNoNews(!showNoNews);
+      .catch((err) => console.log("Error fetching data:", err));
   }
   return (
     <>
       <div className="full-container">
         <h1>Breaking News!</h1>
         <Form updateCategory={categoryChange} />
-        {showNoNews ? (<div className="no-news">
-          <p>No news found.</p>
-        </div>) : (<div className="result">
-          {news.map((item, key) => {
-            return <NewsCard data={item} key={item.article_id} />
-          })}
-        </div>)
-        }
+        {showNoNews ? (
+          <div className="no-news">
+            <p>No news found.</p>
           </div>
+        ) : (
+          <div className="result">
+            {news.map((item, key) => {
+              return <NewsCard data={item} key={item.article_id} />;
+            })}
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,37 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // handleCategorySubmit passes submitted category from user. it is equivalent to categoryChange function
-function Form({updateCategory}) {
-   const [selectedCategory, setSelectedCategory] = useState("");
+function Form({ updateCategory }) {
+  const [selectedCategory, setSelectedCategory] = useState("General");
   const handleChange = (e) => {
     console.log(e.target.value);
     setSelectedCategory(e.target.value);
   };
   function handleSubmit(e) {
-      e.preventDefault();
+    e.preventDefault();
     // updateCategory is App.jsx categoryChange function
     updateCategory(selectedCategory);
   }
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="form">
-                {/* hard coded value attribute to match data.category */}
-                                    <label>Choose a category: </label>
-                <select value={selectedCategory} onChange={handleChange}>
-                    {/* hardcoded calue gets stored in selectedCategory */}
-                    <option value="General">General</option>
-                    <option value="World">World</option>
-                    <option value="Nation">Nation</option>
-                    <option value="Business">Business</option>
-                      <option value="Technology">Technology</option>
-                      <option value="Entertainment">Entertainment</option>
-                    <option value="Sports">Sports</option>
-                    <option value="Science">Science</option>
-                    <option value="Health">Health</option>
-                </select> 
-                <button type="submit">Get News</button>
-        </form>
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="form">
+        {/* hard coded value attribute to match data.category */}
+        <label>Choose a category: </label>
+        <select value={selectedCategory} onChange={handleChange}>
+          {/* hardcoded calue gets stored in selectedCategory */}
+          <option value="General">General</option>
+          <option value="World">World</option>
+          <option value="Nation">Nation</option>
+          <option value="Business">Business</option>
+          <option value="Technology">Technology</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Sports">Sports</option>
+          <option value="Science">Science</option>
+          <option value="Health">Health</option>
+        </select>
+        <button type="submit">Get News</button>
+      </form>
+    </>
+  );
 }
 export default Form;
